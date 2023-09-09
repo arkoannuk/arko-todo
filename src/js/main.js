3efb8634f0e5bf131969b6163ff56d337884c77d
arkoa <arkoannuk+prog@gmail.com>
2023-09-09 16:24:44 +0300

    Major commit

--- src/js/main.js
+++ src/js/main.js
@@ -22,10 +22,10 @@ class Project {
         this.name = name
         this.todoList = []
         this.displayController = new DisplayController()
+        this.sortedTodoList = []
     }
 
     completeTodo(checked, todoIndex, projectIndex) {
-        console.log(todoIndex)
         let todo = this.todoList[todoIndex]
         if (checked) {
             todo.status = true
@@ -38,7 +38,7 @@ class Project {
     addTodo(title, desc, date, starred, status, projectIndex) {
         this.todoList.push(new Todo(title, desc, date, starred, status))
         let todoIndex = this.todoList.length - 1
-        this.displayController.addTodoCard(title, desc, date, starred, status, todoIndex, projectIndex)
+        this.displayController.addTodoCard(title, desc, date, starred, status, todoIndex, projectIndex, true)
     }
 
     deleteTodo(todoIndex, todoCard) {
@@ -55,6 +55,33 @@ class Project {
 
         this.displayController.editTodoCard(title, desc, date, starred, todoIndex, projectIndex)
     }
+
+    sortTodoList(sortableTodoList) {
+        const sortedTasks = sortableTodoList.sort((a, b) => {
+            // First, compare the "status" values
+            if (b.status !== a.status) {
+                return a.status - b.status;
+            }
+
+            // Next, compare the "starred" values
+            if (b.starred !== a.starred) {
+                return b.starred - a.starred;
+            }
+
+            // If both have the same "starred" value, then compare by date
+            if (a.date && !b.date) {
+                return -1; // a comes first because it has a date
+            } else if (!a.date && b.date) {
+                return 1; // b comes first because it has a date
+            } else if (a.date && b.date) {
+                // If both have dates, compare them
+                return new Date(a.date) - new Date(b.date);
+            }
+            // If both have the same "starred" value and no dates, maintain their original order
+            return 0;
+        });
+        return sortedTasks
+    }
 }
 
 class DisplayController {
@@ -66,9 +93,8 @@ class DisplayController {
         if (checked && showCompleted != true) {
             todoCard.style.display = 'none'
         } else {
-            todoCard.style.display = 'block'
+            todoCard.style.display = 'show'
         }
-        console.log(todoCard)
     }
 
     editTodoCard(title, desc, date, starred, todoIndex, projectIndex) {
@@ -122,14 +148,17 @@ class DisplayController {
         let deletableProject = projectSelector.value
         let optionToRemove = projectSelector.querySelector(`option[value="${deletableProject}"]`);
         projectSelector.removeChild(optionToRemove);
+
+        let options = projectSelector.getElementsByTagName('option');
+        for (let i = 0; i < options.length; i++) { // Update remaining selector values
+                options[i].value = i
+        }
     }
 
     showSelectedProject() {
         let projectIndex = document.getElementById('projectSelector').value
         let showAll = document.querySelector('#showAll').checked
         let showCompleted = document.querySelector('#showCompleted').checked
-        let selectedProject = this.projectList[projectIndex]
-        let selectedTodoList = selectedProject.todoList
 
         // This re-renders all TODOs even if showAll is already True upon Project change (TODO: optimize)
         const myNode = document.getElementById("todoList");
@@ -138,15 +167,32 @@ class DisplayController {
         }
 
         if (showAll) {
-            this.projectList.forEach((project, index) => {
-                let projectIndex = index
-                project.todoList.forEach((todo, index) => {
-                    this.displayController.addTodoCard(todo.title, todo.desc, todo.date, todo.starred, todo.status, index, projectIndex)
-                })
+            this.projectList.forEach((project, projectIndex) => {
+                let sortedTodoList = project.sortTodoList(project.todoList)
+                let projectName = document.createElement('h3');
+                projectName.classList.add('mt-2', 'text-center', "text-secondary")
+                projectName.textContent = `${project.name}`
+
+
+                sortedTodoList.forEach((todo, todoIndex) => {
+                    if (todoIndex === 0) {
+                        document.getElementById('todoList').appendChild(projectName);
+                    }
+                    this.displayController.addTodoCard(todo.title, todo.desc, todo.date, todo.starred, todo.status, todoIndex, projectIndex, false);
+                });
+                if (sortedTodoList.length === 0) {
+                    document.getElementById('todoList').appendChild(projectName);
+                }
             })
         } else {
-            selectedTodoList.forEach((todo, index) => {
-                this.displayController.addTodoCard(todo.title, todo.desc, todo.date, todo.starred, todo.status, index, projectIndex)
+            let selectedProject = this.projectList[projectIndex]
+            let projectName = document.createElement('h3');
+            projectName.classList.add('mt-2', 'text-center', "text-secondary")
+            projectName.textContent = `${selectedProject.name}`
+
+            document.getElementById('todoList').appendChild(projectName);
+            selectedProject.sortTodoList(selectedProject.todoList).forEach((todo, index) => {
+                this.displayController.addTodoCard(todo.title, todo.desc, todo.date, todo.starred, todo.status, index, projectIndex, false)
             })
         }
 
@@ -157,14 +203,14 @@ class DisplayController {
         });
         completedTodoCards.forEach((todoCard) => {
             if (showCompleted) {
-                todoCard.style.display = 'show' // I assume you meant 'block' instead of 'show'
+                todoCard.style.display = 'show'
             } else {
                 todoCard.style.display = 'none'
             }
         })
     }
 
-    addTodoCard(title, desc, date, starred, status, todoIndex, projectIndex) {
+    addTodoCard(title, desc, date, starred, status, todoIndex, projectIndex, newCard) {
         const todoCardTemp = document.getElementById('todoCardTemp').content.cloneNode(true)
         const todoCard = todoCardTemp.querySelector('.todoCard')
         todoCard.dataset.projectIndex = projectIndex
@@ -188,21 +234,27 @@ class DisplayController {
 
         if (date !== '') {
             todoDate.textContent = ` ${date}`
-            todoDate.style.display = 'block'
+            todoDate.style.display = 'show'
         } else {
             todoDate.textContent = ` ${date}`
             todoDate.style.display = 'none'
         }
 
         if (starred) {
-            todoStarred.style.display = 'block'
+            todoStarred.style.display = 'show'
             todoEditBtn.classList.remove('ms-auto')
         } else {
             todoStarred.style.display = 'none'
             todoEditBtn.classList.add('ms-auto')
         }
 
-        document.getElementById('todoList').appendChild(todoCardTemp)
+        if (newCard) {
+
+            const todoList = document.getElementById('todoList');
+            todoList.insertBefore(todoCardTemp, todoList.firstChild);
+        } else {
+            document.getElementById('todoList').appendChild(todoCardTemp)
+        }
     }
 
     showEditModal(todoCard) {
@@ -240,9 +292,9 @@ class App {
         document.getElementById('projectForm').addEventListener('submit', this.submitProjectForm.bind(this))
         document.getElementById('projectForm').addEventListener('submit', this.displayController.showSelectedProject.bind(this))
         document.getElementById('projectSelector').addEventListener('change', this.displayController.showSelectedProject.bind(this))
-        document.getElementById('showAll').addEventListener('change', this.displayController.showSelectedProject.bind(this))
-        document.getElementById('editTodoForm').addEventListener('submit', this.submitEditTodoForm.bind(this))
+        document.getElementById('showAll').addEventListener('change', this.displayController.showSelectedProject.bind(this));
         document.getElementById('showCompleted').addEventListener('click', this.displayController.showSelectedProject.bind(this))
+        document.getElementById('editTodoForm').addEventListener('submit', this.submitEditTodoForm.bind(this))
         document.addEventListener('click', (event) => {
             const todoDeleteBtn = event.target.closest('.todoDeleteBtn')
             const todoEditBtn = event.target.closest('.todoEditBtn')
@@ -260,10 +312,6 @@ class App {
                 this.displayController.showEditModal(todoCard)
             }
         })
-    }
-
-    showCompleted() {
-        console.log("test")
     }
 
     changeTodoStatus(todoStatusBtn, todoCard) {
@@ -341,13 +389,29 @@ class App {
         this.addProject('Project B')
         this.projectList[0].addTodo('Task 1', 'Description for Task 1', '', false, false, 0)
         this.projectList[0].addTodo('Task 2', 'Description for Task 2', '2023-09-15', false, false, 0);
-        this.projectList[1].addTodo('Task 3', 'Description for Task 3', '', true, false, 1);
+        this.projectList[1].addTodo('Task 3', 'Description for Task 3', '', false, false, 1);
         this.projectList[1].addTodo('Task 33', 'Description for Task 3', '', true, false, 1);
-        this.projectList[1].addTodo('Task 4', 'Description for Task 4', '2023-09-20', true, false, 1);
+        this.projectList[1].addTodo('Task 4', 'Description for Task 4', '2023-09-20', false, false, 1);
+        this.projectList[1].addTodo('Task 44', 'Description for Task 4', '2023-09-20', true, false, 1);
+        this.projectList[1].addTodo('Task 5', 'Description for Task 4', '2023-06-10', true, false, 1);
         this.projectList[2].addTodo('Task 5', 'Description for Task 5', '2023-09-21', true, false, 2);
+
         console.log(this.projectList)
+    }
+
+    callOutSort() {
+        // Set the projectSelector value to "0" (Default)
+        document.getElementById('projectSelector').value = '0';
+
+        // Trigger the onChange event of projectSelector
+        const projectSelector = document.getElementById('projectSelector');
+        const event = new Event('change');
+        projectSelector.dispatchEvent(event);
     }
 }
 
 const app = new App()
 app.createSampleData()
+app.callOutSort()
+
+// TODO: deleting project A and then project B does not work
